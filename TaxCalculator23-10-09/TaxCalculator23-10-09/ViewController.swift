ddeebc7314402350cea091dc5a620b1ba7e84148
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-10 21:05:30 +0900

    :sparkles: UI部品のレイアウトを作成しコードと紐付け

--- TaxCalculator23-10-09/TaxCalculator23-10-09/ViewController.swift
+++ TaxCalculator23-10-09/TaxCalculator23-10-09/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var excludingTaxTextField: UITextField!
+    @IBOutlet private weak var taxRateTextField: UITextField!
+    @IBOutlet private weak var includingTaxLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
